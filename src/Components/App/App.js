78db65857b8d65722dab29825060ece28dcc9f4b
Erin Black <erin.black@fieldnation.com>
2018-06-22 14:34:43 -0500

    got array printing li onto the dom

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,10 +4,41 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      starList: ['hadar', 'gacrux', 'fomalhout'],
+    };
+  }
+starListItemArray
+
   render() {
+
+    let starListItemArray = [];
+    
+    for(let i=0; i < this.state.starList.length; i++){
+
+      const starListItem =  <li>{this.state.starList[i]}</li>;
+      starListItemArray = [
+        ...starListItemArray,
+        starListItem,
+      ]
+    } 
     return (
-      <div >
+      <div>
+        <div >
+           this.state {JSON.stringify(this.state)};
+        </div>
+        <div>
+         this.state.starlist{this.state.starList}
+       </div>
+       <ul>
+         <li>
+           {starListItemArray}
+         </li>
+       </ul>
       </div>
+      
     );
   }
 }
